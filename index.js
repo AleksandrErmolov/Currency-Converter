f2eab363e71dfc9d55012fcf361289bb7e51ceb8
unknown <ermolov1337@gmail.com>
2021-10-30 12:41:54 +0300

    release button change

--- index.js
+++ index.js
@@ -80,13 +80,13 @@ function changeColorTo(event) {
 //Меняем цвет кнопок у левой стороны
 function changeColorFrom(event) {
     let button = event.target
-    
+
 
     if (button.tagName === "BUTTON") {
         let oldButton = document.querySelector(".money__change__button-active-from")
         oldButton.classList.remove("money__change__button-active-from")
         button.classList.add("money__change__button-active-from")
-    } 
+    }
 }
 
 
@@ -97,13 +97,11 @@ function changeSelectTo() {
 }
 
 
-
 function changeSelectFrom() {
     let oldButton = document.querySelector(".money__change__button-active-from")
     oldButton.classList.remove("money__change__button-active-from")
     rightSelect.classList.add("money__change__button-active-from")
 }
-
 
 
 //Генерируем значение по кнопкам в INPUT
@@ -139,7 +137,7 @@ function renderCurrencyOnButton() {
 
 
 function generalChange() {
-   
+
     let left = document.querySelector(".money__change__button-active-to").value
     let right = document.querySelector(".money__change__button-active-from").value
 
@@ -161,39 +159,51 @@ function generalChange() {
 
 
 function changeInputFrom() {
-  
-        let left = document.querySelector(".money__change__button-active-to").value
-        let right = document.querySelector(".money__change__button-active-from").value
 
-        async function startProgram() {
-            const resp = await fetch(`https://api.exchangerate.host/convert?from=${right}&to=${left}`)
-            const data = await resp.json()
-            return data
-        }
-        startProgram()
-            .then((data) => {
-                console.log(data.result)
-                inputTo.value = (1 * inputFrom.value * data.result).toFixed(4)
-            })
+    let left = document.querySelector(".money__change__button-active-to").value
+    let right = document.querySelector(".money__change__button-active-from").value
+
+    async function startProgram() {
+        const resp = await fetch(`https://api.exchangerate.host/convert?from=${right}&to=${left}`)
+        const data = await resp.json()
+        return data
+    }
+    startProgram()
+        .then((data) => {
+            console.log(data.result)
+            inputTo.value = (1 * inputFrom.value * data.result).toFixed(4)
+        })
 }
 
 
-
 function changeCurrencyButton() {
-    const p1 = document.querySelector(".calculate-to")
-    const p2 = document.querySelector(".calculate-from")
-    let div = document.querySelector(".calculate")
 
+    async function test() {
+        const resp = await fetch("https://api.exchangerate.host/latest")
 
-    if (div.classList.contains("active") === false) {
-        div.classList.toggle("active")
-        div.style.flexdDirection = "row-reverse"
-        p1.innerText = "Хочу приобрести"
-        p2.innerText = "У меня есть"
-
-    } else {
-        div.classList.toggle("active")
-        p1.innerText = "У меня есть"
-        p2.innerText = "Хочу приобрести"
+        const data = await resp.json()
+        return data
     }
+    test()
+        .then((money) => {
+            const p1 = document.querySelector(".calculate-to")
+            const p2 = document.querySelector(".calculate-from")
+            let div = document.querySelector(".calculate")
+
+            if (div.classList.contains("active") === false) {
+                div.classList.toggle("active")
+                div.style.flexdDirection = "row-reverse"
+                p1.innerText = "Хочу приобрести"
+                p2.innerText = "У меня есть"
+
+            } else {
+
+                div.classList.toggle("active")
+                p1.innerText = "У меня есть"
+                p2.innerText = "Хочу приобрести"
+            }
+        })
+        .catch((eror) => {
+            alert("Ошибка сервера")
+        })
 }
